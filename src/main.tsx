0a2504fa725025083db7c49c9941e1b6d93c5613
prajwalmandlik <prajwalmandlik7@gmail.com>
2024-11-08 17:45:59 +0530

    feat: add date-fns dependency and update API base URLs to port 5000

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import '@/index.css'
 
-import { ThemeProvider } from '@/components/theme-provider'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient = new QueryClient()
-import '@/index.css'
 import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from '@/components/theme-provider'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
