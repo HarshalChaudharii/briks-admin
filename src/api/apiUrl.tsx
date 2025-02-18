5dcdd952bf6cadf0db9cc8f42aa2fb9699655d4d
HarshalChaudharii <theguyharshal@gmail.com>
2025-02-18 12:02:53 +0530

    'update'

--- src/api/apiUrl.tsx
+++ src/api/apiUrl.tsx
@@ -1,8 +1,6 @@
 // export const BASE_URL = 'http://localhost:5000/api/v1'
 // export const IMAGE_BASE_URL = 'http://localhost:5000'
 
-// export const BASE_URL = 'http://52.186.139.199:5000/api/v1'
-// export const IMAGE_BASE_URL = 'http://52.186.139.199:5000'
 export const BASE_URL = 'http://app.brickinsight.in/api/v1'
 export const IMAGE_BASE_URL = 'http://app.brickinsight.in'
 
